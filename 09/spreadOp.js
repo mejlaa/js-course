8961517fdec2bf1b2ca6a515d99d0ac6986068d1
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-03-24 14:20:13 +0100

    spread op

--- 09/spreadOp.js
+++ 09/spreadOp.js
@@ -42,22 +42,54 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
-  orderPasta(ing1, ing2, ing3) {
+  orderPasta: function (ing1, ing2, ing3) {
     console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
     );
-  },
-  orderPizza(mainIngredient, ...otherIngredient) {
-    console.log(mainIngredient);
-    console.log(otherIngredient);
   },
 };
 
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+// console.log(badNewArr);
 
 const newArr = [1, 2, ...arr];
-console.log(newArr);
+// console.log(newArr);
 
-console.log(...newArr);
+// console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+//copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// console.log(menu);
+
+//iterables:array, strings, maps, sets. NOT objects
+const str = 'Mejla';
+const letters = [...str, ' ', 'U. Suljovic'];
+// console.log(letters.join(''));
+// console.log(...str);
+
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3'),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+//objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+// console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+//changing name of restaurant
+restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurantCopy.name);
