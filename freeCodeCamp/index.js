2ee9287f0f22ba80129be4afcecdfae6b64d70ed
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-05-08 13:17:52 +0200

    freecode

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -1,3 +1,4 @@
+/*
 // Declare the myGlobal variable below this line
 const myGlobal = 10;
 function fun1() {
@@ -123,4 +124,186 @@ function orderMyLogic(val) {
   }
 }
 
-orderMyLogic(7);
+orderMyLogic(7); */
+
+const names = [
+  'Hole-in-one!',
+  'Eagle',
+  'Birdie',
+  'Par',
+  'Bogey',
+  'Double Bogey',
+  'Go Home!',
+];
+
+function golfScore(par, strokes) {
+  if (strokes == 1) return names[0];
+  else if (strokes <= par - 2) return names[1];
+  else if (strokes == par - 1) return names[2];
+  else if (strokes == par) return names[3];
+  else if (strokes == par + 1) return names[4];
+  else if (strokes == par + 2) return names[5];
+  else if (strokes >= par + 3) return names[6];
+}
+
+// console.log(golfScore(1, 4));
+
+function caseInSwitch(val) {
+  let answer = '';
+
+  switch (val) {
+    case 1:
+      return 'alpha';
+      break;
+    case 2:
+      return 'beta';
+      break;
+    case 3:
+      return 'gamma';
+      break;
+    case 4:
+      return 'delta';
+      break;
+    default:
+      return 'there is nothing';
+      break;
+  }
+
+  return answer;
+}
+
+// console.log(caseInSwitch(5));
+
+function switchOfStuff(val) {
+  let answer = '';
+
+  switch (val) {
+    case 'a':
+      return 'apple';
+    case 'b':
+      return 'bird';
+      break;
+    case 'c':
+      return 'cat';
+      break;
+    default:
+      return 'stuff';
+      break;
+  }
+
+  return answer;
+}
+
+// console.log(switchOfStuff('6'));
+
+function sequentialSizes(val) {
+  let answer = '';
+
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      return 'Low';
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return 'Mid';
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return 'High';
+      break;
+  }
+
+  return answer;
+}
+
+// console.log(sequentialSizes(7));
+
+function chainToSwitch(val) {
+  let answer = '';
+
+  switch (val) {
+    case 'bob':
+      return 'Marley';
+      break;
+    case 42:
+      return 'The Answer';
+      break;
+    case 1:
+      return 'There is no #1';
+      break;
+    case 99:
+      return 'Missed me by this much!';
+      break;
+    case 7:
+      return 'Ate Nine';
+      break;
+  }
+
+  return answer;
+}
+
+// console.log(chainToSwitch('bob'));
+
+function isLess(a, b) {
+  //   if (a < b) return true;
+  //   else return false;
+  //same as this but cleaner
+  return a <= b;
+}
+
+// console.log(isLess(10, 15));
+
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+  }
+
+  if (count > 0) return count + ' Bet';
+  else return count + ' Hold';
+}
+
+// console.log(cc(2));
+// console.log(cc(7));
+// console.log(cc('K'));
+// console.log(cc('A'));
+
+const myDog = {
+  name: 'doggy',
+  legs: 4,
+  tails: 1,
+  friends: ['mejla', 'suad', 'nesto'],
+};
+
+const testObj = {
+  12: 'Nameth',
+  16: 'Montana',
+  19: 'Unites',
+};
+const playerNumber = 16;
+const player = testObj[playerNumber];
+
+// console.log(player);
+testObj['12'] = 'Ugljanin';
+// console.log(testObj);
+
+testObj['22'] = 'Suljovic';
+// console.log(testObj);
