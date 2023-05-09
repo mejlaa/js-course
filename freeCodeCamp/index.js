82f748ee58985cda6c319a7135e42e3abe55adbf
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-05-09 13:14:10 +0200

    loops

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -419,3 +419,32 @@ for (let i = 1; i < 10; i += 2) {
 for (let i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+function multiplyAll(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+
+multiplyAll([
+  [1, 2],
+  [3, 4],
+  [5, 6, 7],
+]);
+
+const myArray1 = [];
+let ii = 10;
+do {
+  myArray1.push(ii);
+  ii++;
+} while (ii < 5);
