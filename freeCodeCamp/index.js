c63c9f8ecaf5b396115de04df497d0172b80dceb
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-05-09 12:27:34 +0200

    loops freecodecamp

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -362,3 +362,60 @@ function checkObj(obj, checkProp) {
   else return 'Not Found';
 }
 // console.log(checkObj(lookup, 'mejla'));
+
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette'],
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: [],
+  },
+  5439: {
+    albumTitle: 'ABBA Gold',
+  },
+};
+
+function updateRecords(records, id, prop, value) {
+  if (value === '') {
+    delete records[id][prop];
+  } else if (prop !== 'tracks' && value !== '') {
+    records[id][prop] = value;
+  } else if (prop === 'track' && value !== '') {
+    if (records[id].hasOwnProperty('tracks') === false) {
+      records[id][prop] = [];
+    }
+    records[id][prop].push(value);
+  }
+
+  return records;
+}
+
+// console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+// console.log(updateRecords(recordCollection, 5439, 'artist', ''));
+const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
